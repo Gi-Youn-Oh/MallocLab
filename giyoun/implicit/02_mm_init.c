552a18771fc6e29685e222a52837e34d67046aca
Giyoun Oh <okrldbs@naver.com>
2022-10-30 18:24:35 +0900

    1com

--- giyoun/implicit/02_mm_init.c
+++ giyoun/implicit/02_mm_init.c
@@ -1,19 +1,19 @@
-int mm_init(void)
-{
-    /*Create the initial empty heap*/
-    if ((heap_listp = mem_sbrk(4*WSIZE == (void*)-1)
-        return -1;
-    PUT(heap_listp, 0);    // Alignment padding
-    PUT(heap_listp + (1*WSIZE), PACK(DSIZE, 1)); // Prologue header
-    PUT(heap_listp + (2*WSIZE), PACK(DSIZE, 1)); // Prologue footer
-    PUT(heap_listp + (3*WSIZE), PACK(0, 1)); // Epilogue hearder
-    heap_listp += (2*WSIZE);
+// int mm_init(void)
+// {
+//     /*Create the initial empty heap*/
+//     if ((heap_listp = mem_sbrk(4*WSIZE == (void*)-1)
+//         return -1;
+//     PUT(heap_listp, 0);    // Alignment padding
+//     PUT(heap_listp + (1*WSIZE), PACK(DSIZE, 1)); // Prologue header
+//     PUT(heap_listp + (2*WSIZE), PACK(DSIZE, 1)); // Prologue footer
+//     PUT(heap_listp + (3*WSIZE), PACK(0, 1)); // Epilogue hearder
+//     heap_listp += (2*WSIZE);
 
-    /* Extend the empty heap with a free block of CHUNKSIZE bytes*/
-    if (extend_heap(CHUNKSIZE/WSIZE) == NULL)
-        return -1;
-    return 0; 
-}
+//     /* Extend the empty heap with a free block of CHUNKSIZE bytes*/
+//     if (extend_heap(CHUNKSIZE/WSIZE) == NULL)
+//         return -1;
+//     return 0; 
+// }
 
 /* 
  * mm_init - initialize the malloc package.
--- giyoun/implicit/09_realloc.c
+++ giyoun/implicit/09_realloc.c
@@ -6,18 +6,19 @@ void *mm_realloc(void *ptr, size_t size) {
     // 블록의 크기를 늘리는 것이면 
     // 핵심은, 이미 할당된 블록의 사이즈를 직접 건드리는 것이 아니라, 요청한 사이즈 만큼의 블록을 새로 메모리 공간에 만들고 현재의 블록을 반환하는 것이다.
     // 해당 블록의 사이즈가 이 정도로 변경되었으면 좋겠다는 것이 size, 
-    void *oldptr = ptr;                                                     // 크기를 조절하고 싶은 힙의 시작 포인터
-    void *newptr;                                                           // 크기 조절 뒤의 새 힙의 시작 포인터
-    size_t copySize;                                                        // 복사할 힙의 크기
+    void *oldptr = ptr;     // 크기를 조절하고 싶은 힙의 시작 포인터
+    void *newptr;           // 크기 조절 뒤의 새 힙의 시작 포인터
+    size_t copySize;        // 복사할 힙의 크기
     
-    newptr = mm_malloc(size);                                               // place를 통해 header, footer가 배정된다.
+    newptr = mm_malloc(size);  // place를 통해 header, footer가 배정된다.
     if (newptr == NULL) {
         return NULL;
     }
     
-    copySize = GET_SIZE(HDRP(oldptr));                                      // 원래 블록의 사이즈
+    copySize = GET_SIZE(HDRP(oldptr));  // 원래 블록의 사이즈
     
-    if (size < copySize) {                                                  // 만약 블록의 크기를 줄이는 것이라면 size만큼으로 줄이면 된다. copySize - size 공간의 데이터는 잘리게 된다. 밑의 memcpy에서 잘린 만큼의 데이터는 복사되지 않는다.
+    if (size < copySize) {    // 만약 블록의 크기를 줄이는 것이라면 size만큼으로 줄이면 된다. 
+    // copySize - size 공간의 데이터는 잘리게 된다. 밑의 memcpy에서 잘린 만큼의 데이터는 복사되지 않는다.
         copySize = size;
     }
     
